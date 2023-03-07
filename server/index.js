4af2224999ad6a421240d33d1cd3238db24a030a
madhusudhan <chmadhus97@gmail.com>
2023-03-08 00:53:50 +0530

    finished auth flow

--- server/index.js
+++ server/index.js
@@ -1,9 +1,42 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys')
+require('./models/user');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'madhu'});
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+// register regenerate & save after the cookieSession middleware initialization
+app.use(function(request, response, next) {
+    if (request.session && !request.session.regenerate) {
+        request.session.regenerate = (cb) => {
+            cb()
+        }
+    }
+    if (request.session && !request.session.save) {
+        request.session.save = (cb) => {
+            cb()
+        }
+    }
+    next()
+})
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
